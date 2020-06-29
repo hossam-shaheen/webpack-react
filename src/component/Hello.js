0a97c84d03b590e6e76b2289881ba2d8956cbbd4
hossam shaheen <hossameldinshaheen@gmail.com>
2020-06-29 23:56:23 +0200

    first commit

---
+++ src/component/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import  "./hello.css";
+
+export const  HelloMessage = () => {
+      return (
+        <div>
+          <h2>Hello world!</h2>         
+        </div>
+      );    
+  }
+  
+ 
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve("dist"),
+    filename: "[name].[contenthash].js"
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ]
+  },
+  plugins: [
+
+    new OptimizeCssAssetsPlugin(),
+    new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: '[name].[hash].css',
+        chunkFilename: '[id].css',
+      }),
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+    })
+ ]
+};
